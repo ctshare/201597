acdff7c1ae8cf901c8ef7d871055433922e794da
ctshare <168668532+ctshare@users.noreply.github.com>
2024-05-02 09:34:04 -0400

    Update test_A201.py

--- helper/test_A201.py
+++ helper/test_A201.py
@@ -1,11 +1,18 @@
+"""
+For homeworks autograding@A201+A202
+"""
+
 from io import StringIO
 from IPython import get_ipython
 from IPython.core.magic import Magics, cell_magic, magics_class, register_cell_magic
 
 @magics_class
-class MyMagic(Magics):
+class A201_autograde_magic(Magics):
     @cell_magic
     def A201_autograde_stdin(self, line, cell):
+        """
+        Transform a cell into a function with user inputs packed into a list as the function parameter.
+        """
         self.shell.run_cell(cell)
         i = 0
         new_codeblock = ""
@@ -21,6 +28,15 @@ class MyMagic(Magics):
             else:
                 new_codeblock += codeline
         self.shell.run_cell(new_codeblock)
+    
+    @cell_magic
+    def A201_autograde_stdout(self, variable, cell):
+        """
+        Capture the output of a code cell without stdin and suppresing the display.
+        """
+        get_ipython().magics_manager.magics['cell']['capture'](variable, cell)
+        globals()["os"] = __import__("os")
+        globals()[variable].show()
         
 ipy = get_ipython()
-ipy.register_magics(MyMagic)
+ipy.register_magics(A201_autograde_magic)
